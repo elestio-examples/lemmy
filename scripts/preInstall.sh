ae907337a89623028fc24df8fdf83cacc56f7327
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-11 16:14:26 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -5,7 +5,28 @@ set -o allexport; source .env; set +o allexport;
 mkdir -p ./pictrs
 chown -R 991:991 ./pictrs
 
-sed -i "s~USER_TO_CHANGE~${ADMIN_EMAIL}~g" ./lemmy.hjson
+filename="./post.txt"
+
+SMTP_LOGIN=""
+SMTP_PASSWORD=""
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Extract the values after the flags (-e)
+  values=$(echo "$line" | grep -o '\-e [^ ]*' | sed 's/-e //')
+
+  # Loop through each value and store in respective variables
+  while IFS= read -r value; do
+    if [[ $value == SMTP_LOGIN=* ]]; then
+      SMTP_LOGIN=${value#*=}
+    elif [[ $value == SMTP_PASSWORD=* ]]; then
+      SMTP_PASSWORD=${value#*=}
+    fi
+  done <<< "$values"
+
+done < "$filename"
+
+sed -i "s~ADMIN_EMAIL~${ADMIN_EMAIL}~g" ./lemmy.hjson
 sed -i "s~PASSWORD_TO_CHANGE~${ADMIN_PASSWORD}~g" ./lemmy.hjson
 sed -i "s~DOMAIN_TO_CHANGE~${DOMAIN}~g" ./lemmy.hjson
 sed -i "s~API_KEY_TO_CHANGE~${API_KEY}~g" ./lemmy.hjson
@@ -13,5 +34,7 @@ sed -i "s~POSTGRES_USER~${POSTGRES_USER}~g" ./lemmy.hjson
 sed -i "s~POSTGRES_PASSWORD~${POSTGRES_PASSWORD}~g" ./lemmy.hjson
 sed -i "s~SMTP_HOST~${SMTP_HOST}~g" ./lemmy.hjson
 sed -i "s~SMTP_PORT~${SMTP_PORT}~g" ./lemmy.hjson
-sed -i "s~SMTP_FROM~${SMTP_FROM_EMAIL}~g" ./lemmy.hjson
+sed -i "s~SMTP_LOGIN~${SMTP_LOGIN}~g" ./lemmy.hjson
+sed -i "s~SMTP_PASSWORD~${SMTP_PASSWORD}~g" ./lemmy.hjson
+sed -i "s~SMTP_FROM~${SMTP_LOGIN}~g" ./lemmy.hjson
 sed -i "s~SMTP_AUTH_STRATEGY~${SMTP_AUTH_STRATEGY}~g" ./lemmy.hjson
