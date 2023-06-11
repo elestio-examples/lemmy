4e33067bf29bef8a965a5f693d84af284bed50a2
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-11 09:53:21 +0300

    fix

--- README.md
+++ README.md
@@ -13,5 +13,5 @@ Deploy Lemmy server with CI/CD on Elestio
 You can open Lemmy UI here:
 
     URL: https://[CI_CD_DOMAIN]
-    email: [ADMIN_EMAIL]
+    email: admin
     password: [ADMIN_PASSWORD]
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -10,5 +10,5 @@ sed -i "s~USER_TO_CHANGE~${ADMIN_EMAIL}~g" ./lemmy.hjson
 sed -i "s~PASSWORD_TO_CHANGE~${ADMIN_PASSWORD}~g" ./lemmy.hjson
 sed -i "s~DOMAIN_TO_CHANGE~${DOMAIN}~g" ./lemmy.hjson
 sed -i "s~API_KEY_TO_CHANGE~${API_KEY}~g" ./lemmy.hjson
-sed -i "s~POSTGRES_USER_TO_CHANGE~${POSTGRES_USER}~g" ./lemmy.hjson
-sed -i "s~POSTGRES_PASSWORD_TO_CHANGE~${POSTGRES_PASSWORD}~g" ./lemmy.hjson
+sed -i "s~POSTGRES_USER~${POSTGRES_USER}~g" ./lemmy.hjson
+sed -i "s~POSTGRES_PASSWORD~${POSTGRES_PASSWORD}~g" ./lemmy.hjson
