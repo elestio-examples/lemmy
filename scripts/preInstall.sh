9b41f2b779664c13dd760315e945be5fff9e5619
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-11 10:13:59 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -12,3 +12,7 @@ sed -i "s~DOMAIN_TO_CHANGE~${DOMAIN}~g" ./lemmy.hjson
 sed -i "s~API_KEY_TO_CHANGE~${API_KEY}~g" ./lemmy.hjson
 sed -i "s~POSTGRES_USER~${POSTGRES_USER}~g" ./lemmy.hjson
 sed -i "s~POSTGRES_PASSWORD~${POSTGRES_PASSWORD}~g" ./lemmy.hjson
+sed -i "s~SMTP_HOST~${SMTP_HOST}~g" ./lemmy.hjson
+sed -i "s~SMTP_PORT~${SMTP_PORT}~g" ./lemmy.hjson
+sed -i "s~SMTP_FROM~${SMTP_FROM_EMAIL}~g" ./lemmy.hjson
+sed -i "s~SMTP_AUTH_STRATEGY~${SMTP_AUTH_STRATEGY}~g" ./lemmy.hjson
