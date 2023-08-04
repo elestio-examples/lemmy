fc449927ce47461469fb3476fc1c8936306f0d3c
Prem chavhan <pchavhan@logiquad.com>
2023-08-04 19:44:20 +0530

    fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cp -rf docker/* ./
+rm -f docker-compose.yml
+mv docker-compose-new.yml docker-compose.yml
 docker buildx build . --output type=docker,name=elestio4test/lemmy:latest | docker load
