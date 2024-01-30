7972f6a16184ab1c941707773c36a60c22e6fadc
Prem chavhan <pchavhan@logiquad.com>
2024-01-30 16:39:19 +0530

    fix

--- tests/backend/build.sh
+++ tests/backend/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+rm -f docker/README.md
 cp -rf docker/* ./
 rm -f docker-compose.yml
-rm -f nginx-new.conf
+rm -f nginx.conf
 mv docker-compose-new.yml docker-compose.yml
 mv nginx-new.conf nginx.conf
 docker buildx build . --output type=docker,name=elestio4test/lemmy:latest | docker load
